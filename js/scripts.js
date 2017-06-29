a7497bbc521c0f3cdae4d6030cfe1202d59d0a83
gabrijela <gabigavric@gmail.com>
2017-06-28 18:02:41 -0700

    made page so the code is concise

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,35 @@
 var add = function(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number;"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = add(number1, number2);
-alert(result);
-
-
 var subtract = function(number1, number2) {
-  return number1 / number2;
-}
-
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = subtract(number1, number2);
-alert(result);
+  return number1 - number2;
+};
 
 var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = multiply(number1, number2);
-alert(result);
-
 var divide = function(number1, number2) {
   return number1 / number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-var result = divide(number1, number2);
-alert(result);
+$(document).ready(function() {
+  $("form#calculator").submit(function() {
+    event.preventDefault();
+    var number1 = parseInt($("#input1").val());
+    var number2 = parseInt($("#input2").val());
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
+  });
+});
